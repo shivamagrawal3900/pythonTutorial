b9fc2b5ac901b925031fec9b442c45c3315564bb
Shivam Agrawal <shivam.agrawal.3900@gmail.com>
2021-03-03 20:43:55 +0530

    fixed corner case

--- day4_list_and_tuple/longest_increasing_sequence.py
+++ day4_list_and_tuple/longest_increasing_sequence.py
@@ -10,5 +10,7 @@ for i in range(len(l1) - 1):
         if current_sequence > max_sequence:
             max_sequence = current_sequence
         current_sequence = 1
-
+else:
+    if current_sequence > max_sequence:
+        max_sequence = current_sequence
 print("Max sequence is: ", max_sequence)
